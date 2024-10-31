e448fcbcb0b5df2bc593fcfb7409cf3b9fef8192
Raja Pandi <114086052+Rawwire@users.noreply.github.com>
2024-10-31 12:38:40 +0530

    Updated resume

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 current_dir= Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file= current_dir/"styles"/"main.css"
-resume_file = current_dir/"assets"/"RajapandiS_Resume1.pdf"
+resume_file = current_dir/"assets"/"RajapandiSresume.pdf"
 profile_pic= current_dir/"assets"/"profile-pic (12).png"
 
 
